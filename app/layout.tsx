06abf5fdab965840f774bcacf61772207529348d
Alfonzo Fong <78888305+alfong12@users.noreply.github.com>
2024-04-08 16:21:49 -0400

    agregado icon LB

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,15 @@ export const metadata = {
   description: 'Prueba Tecnica de Alfonzo Fong',
 };
 
+const LBiconref =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABL0lEQVR4Ae3WAUcEQRTA8aATlAB9hAMcIEIDHKwEi8I2XSy7djnXFxi4DdwBONDWh6hTd7miAiAcA6Rv0YLXvLWTwXriTOfO4DcYj/cfYDYAYKnK44V5QHlmnsQ5wfOW6NwA6Twf4WzvadDtTYdAqQ9wAYdegQtN6v6+CmiKzq2k5aIMmAyCy+lQmtTSggwwX2sDRiwU4Pt+I47jPS0Mw81/DUiShKnFoEVRtL8eAS7ABbiAGTve1V4PjrbtBNSrvbQVMJ/7jc8Z29J+A07yybfhw1bA11tbKqCZAWCQLmBVAtAFRpjSNN2xH0DAGRdgLUB/ySg4Yy1gUX8NKBRpOr1+LL/lDzxrjoMrSeJ9US57bwfVQlNBBhAkzt3xfmt8lgGJZ6MqoItLKC5AByzVD6YrvTfJZELEAAAAAElFTkSuQmCC';
+
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href={LBiconref} />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
